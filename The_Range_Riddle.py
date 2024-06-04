fbf4ea4bfb359a8fbd80ea7e440441e24164664b
bradkerwin <bkerwin101@gmail.com>
2024-06-04 17:14:44 -0400

    Update The_Range_Riddle.py
    fixed a typo in my pseudocode.

--- The_Range_Riddle.py
+++ The_Range_Riddle.py
@@ -2,5 +2,5 @@
 
 days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 for x in range(len(days_of_week)): # This ranges through the length of the list, which is 6 because there are 6 items. It starts from 0 and ends at 6.
-    if x % 2 == 0:  # Creates a condition of the index is even.
+    if x % 2 == 0:  # Creates a condition if the index is even.
         print(days_of_week[x])  # Prints out the items in the list with an even numbered index.
